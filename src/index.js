2cd7b012dac7f2e12568accaebe0e0afecc163cc
IvanDarmovis <ivan.darmovis1@gmail.com>
2022-02-11 18:08:20 +0200

    final commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import template from './templates/card.hbs';
 import './sass/main.scss';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import Notiflix from 'notiflix';
+import throttle from 'lodash.throttle';
 
 const API_KEY = '25607511-28b83b13f0e2975028585da7b';
 const URL = `https://pixabay.com/api/?key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=40`;
@@ -48,15 +49,24 @@ async function onSearch(ev) {
 }
 
 async function onSearchMore(ev) {
-  ev.preventDefault();
-  page += 1;
-  const response = await getPhotos(searchItem, page);
-  console.dir(refs.imgContainer);
-  if (refs.imgContainer.childElementCount < response.totalHits) {
-    const markup = createMarkup(response.hits);
-    refs.imgContainer.insertAdjacentHTML('beforeend', markup);
-    litebox.refresh();
-  } else {
+  if (ev) ev.preventDefault();
+  try {
+    page += 1;
+    const response = await getPhotos(searchItem, page);
+    if (refs.imgContainer.childElementCount < response.totalHits) {
+      const markup = createMarkup(response.hits);
+      refs.imgContainer.insertAdjacentHTML('beforeend', markup);
+      litebox.refresh();
+      const { height: cardHeight } = document
+        .querySelector('.gallery')
+        .firstElementChild.getBoundingClientRect();
+
+      window.scrollBy({
+        top: cardHeight * 2,
+        behavior: 'smooth',
+      });
+    }
+  } catch {
     refs.showMoreBtn.classList.add('hidden');
     Notiflix.Notify.warning("We're sorry, but you've reached the end of search results.");
   }
@@ -64,4 +74,23 @@ async function onSearchMore(ev) {
 
 function createMarkup(arr) {
   return arr.map(el => template(el)).join('\n');
+}
+
+window.addEventListener('scroll', throttle(infiniteScroll, 300));
+
+function infiniteScroll() {
+  const height = document.body.offsetHeight;
+  const screenHeight = window.innerHeight;
+  const scrolled = window.scrollY;
+  const limit = height - screenHeight / 4;
+  const position = scrolled + screenHeight;
+
+  if (position >= limit) {
+    try {
+      onSearchMore();
+    } catch {
+      refs.showMoreBtn.classList.add('hidden');
+      Notiflix.Notify.warning("We're sorry, but you've reached the end of search results.");
+    }
+  }
 }
